9de1d74ffb1599a4faf76f4201a070b19dabda2a
Andres Felipe Arevalo Suarez <36401985+andresfelipear@users.noreply.github.com>
2024-01-20 23:25:47 -0800

    created login layout. Added styling

--- app/src/main/java/com/vancouverparking/parkingapp2/MainActivity.kt
+++ app/src/main/java/com/vancouverparking/parkingapp2/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.vancouverparking.parkingapp2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var loginButton: Button
+
+        loginButton = findViewById(R.id.login_main)
+        loginButton.setOnClickListener {
+            loginButton.setBackgroundColor(getColor(R.color.colorAccent))
+            setContentView(R.layout.activity_login)
+
+        }
+
     }
 }
