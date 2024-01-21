e4e6696a3a969130bcfd8b7b28842c4efa908257
Andres Felipe Arevalo Suarez <36401985+andresfelipear@users.noreply.github.com>
2024-01-21 00:54:17 -0800

    connected main with login & signup activities

--- app/src/main/java/com/vancouverparking/parkingapp2/MainActivity.kt
+++ app/src/main/java/com/vancouverparking/parkingapp2/MainActivity.kt
@@ -4,22 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.vancouverparking.parkingapp2.views.authentication.login.LoginActivity
+import com.vancouverparking.parkingapp2.views.authentication.signup.SignUpActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /**
+
         var loginButton: Button
 
-        loginButton = findViewById(R.id.login_main)
-        loginButton.setOnClickListener {
-            loginButton.setBackgroundColor(getColor(R.color.colorAccent))
-            setContentView(R.layout.activity_login)
-
+        findViewById<Button>(R.id.login).setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
         }
-        */
+
+        findViewById<Button>(R.id.signUp).setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
 
     }
 }
---
+++ app/src/main/java/com/vancouverparking/parkingapp2/views/authentication/signup/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.vancouverparking.parkingapp2.views.authentication.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vancouverparking.parkingapp2.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
