285154e75c808ec845810128636d5b6dd3866cc4
Andres Felipe Arevalo Suarez <36401985+andresfelipear@users.noreply.github.com>
2024-02-01 20:05:58 -0800

    working in the signup countries spinner. Spinner item layout done

---
+++ app/src/main/java/com/vancouverparking/parkingapp2/SignUpVerification.kt
@@ -0,0 +1,13 @@
+package com.vancouverparking.parkingapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpVerification : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_verification)
+    }
+}
